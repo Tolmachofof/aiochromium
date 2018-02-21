011328700c0a280c67134a83f58a4cf42f955068
evtolmachev <tolmachofof@gmail.com>
2018-02-21 15:41:14 +0300

    Structured domains

--- aiochromium/domains/page.py
+++ aiochromium/domains/page.py
@@ -7,14 +7,19 @@ class Page(Domain):
     _DISABLE = 'Page.disable'
     _NAVIGATE = 'Page.navigate'
     _RELOAD = 'Page.reload'
+    _SET_GEOLOCATION_OVERRIDE = 'Page.setGeolocationOverride'
+    _CLEAR_GEOLOCATION_OVERRIDE = 'Page.clearGeolocationOverride'
+    _HANDLE_JAVASCRIPT_DIALOG = 'Page.handleJavaScriptDialog'
     _SCREENSHOT = 'Page.captureScreenshot'
     _FRAME_STOPPED_LOADING = 'Page.frameStoppedLoading'
 
-    def enable(self):
-        return RequestFrame(self._ENABLE)
+    @classmethod
+    def enable(cls):
+        return RequestFrame(cls._ENABLE)
 
-    def disable(self):
-        return RequestFrame(self._DISABLE)
+    @classmethod
+    def disable(cls):
+        return RequestFrame(cls._DISABLE)
 
     @classmethod
     def navigate(
@@ -36,6 +41,31 @@ class Page(Domain):
             {
                 'ignoreCache': ignore_cache,
                 'scriptToEvaluateOnLoad': script_to_eval_on_load
+            }
+        )
+
+    @classmethod
+    def set_geolocation_override(cls, latitude, longitude, accuracy):
+        return cls.create_frame(
+            cls._SET_GEOLOCATION_OVERRIDE,
+            {
+                'latitude': latitude,
+                'longitude': longitude,
+                'accuracy': accuracy
+            }
+        )
+
+    @classmethod
+    def clear_geolocation_override(cls):
+        return cls.create_frame(cls._CLEAR_GEOLOCATION_OVERRIDE)
+
+    @classmethod
+    def handle_javascript_dialog(cls, accept, prompt_text=None):
+        return cls.create_frame(
+            cls._HANDLE_JAVASCRIPT_DIALOG,
+            {
+                'accept': accept,
+                'prompttext': prompt_text
             }
         )
 
